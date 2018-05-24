d4fc2ff515e3052d9e0f6c4a371012a6611d0ed4
Cliff Kang <inlovewithhim@gmail.com>
2018-05-24 11:02:56 -0700

    part of lecture code added for forms

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Bookmark
+from .models import PersonalBookmark
 
-admin.site.register(Bookmark)
+admin.site.register(Bookmark, PersonalBookmark)
