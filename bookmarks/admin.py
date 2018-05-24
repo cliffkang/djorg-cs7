4049fe9a85d65be0349a472a839549c8a261fd9b
Cliff Kang <inlovewithhim@gmail.com>
2018-05-24 14:03:09 -0700

    got project to where lecture ended

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Bookmark
-from .models import PersonalBookmark
+from .models import Bookmark, PersonalBookmark
 
-admin.site.register(Bookmark, PersonalBookmark)
+admin.site.register((Bookmark, PersonalBookmark))
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from .models import Bookmark, PersonalBookmark
 from .forms import BookmarkForm
 
-# Create your views here.
-
 def index(request):
     #debugger import pdb; pdb.set_trace() 
     if request.method == 'POST':
         form = BookmarkForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             # TODO check for request.user to allow for personal bookmarks
             form.save()
         else:
@@ -19,12 +17,12 @@ def index(request):
     context = {}
     pbid = PersonalBookmark.objects.values_list('id')
 
-    context['bookmarks']: Bookmark.objects.exclude(id__in=pbid)
+    context['bookmarks'] = Bookmark.objects.exclude(id__in=pbid)
 
     if request.user.is_anonymous:
-        context['personal_bookmarks']: PersonalBookmark.objects.none()
+        context['personal_bookmarks'] = PersonalBookmark.objects.none()
     else:
-        context['personal_bookmarks']: PersonalBookmark.objects.filter(user=request.user)
+        context['personal_bookmarks'] = PersonalBookmark.objects.filter(user=request.user)
 
     context['form'] = BookmarkForm
     
